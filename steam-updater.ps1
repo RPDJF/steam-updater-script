0a0171a0148f3077f217d4f2719ccb7dce9fce8a
Rui Paulo <86334233+RPDJF@users.noreply.github.com>
2023-09-30 23:28:17 +0200

    Create steam-updater.ps1

---
+++ steam-updater.ps1
@@ -0,0 +1,31 @@
+# Edit this, cowboy
+$steamUsername = "Banatawa";
+
+
+# Check if steamusername was set
+if(!$steamUsername)
+{ 
+    Write-Host "Please edit the script and set your steamid on `$steamUsername variable"
+    pause
+    exit
+}
+
+# Check if there is a login.xml file (please read readme.md)
+if(Test-Path "login.xml")
+{ 
+    Write-Host "Please create a clixml login.xml (read the readme.md)"
+    pause
+    exit
+}
+
+# Fetch steam game list
+$data = [xml](curl "https://steamcommunity.com/id/$steamUsername/games?tab=all&xml=1")
+# For each games, update it
+Write-Host $data
+foreach($game in $data.gamesList.games.game)
+{
+    Write-Host "######## SteamCMD Update - $(Get-Date -Format "yyyyMMdd") ########"
+    Write-Host "Updating $($game.name.'#cdata-section')"
+    Invoke-Expression "cmd.exe /c C:\Opt\steamcmd.exe +Login $($login.username) $($login.GetNetworkCredential().password) +app_update $($game.appID) +validate +quit"
+    Write-Host "Finish"
+}
